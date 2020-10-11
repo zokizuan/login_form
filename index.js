98b978af580f5d5d084c806b7d9d30e2dfe28faf
zokizuan <zoki.zuan@yahoo.com>
2020-10-12 04:07:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+        var particles = Particles.init({
+            selector: '.background',
+          color: '#6c63fe',
+          connectParticles: false,
+          minDistance:100,
+          maxParticles:139,
+          speed:.7,
+          sizeVariations:4,
+        });
+
