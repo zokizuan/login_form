ee249af3d4cceb845917789d8954cc1ecb7dc85e
zokizuan <zoki.zuan@yahoo.com>
2020-10-12 13:47:38 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-        var particles = Particles.init({
-            selector: '.background',
-          color: '#6c63fe',
-          connectParticles: false,
-          minDistance:100,
-          maxParticles:139,
-          speed:.7,
-          sizeVariations:4,
-        });
-
+var particles = Particles.init({
+  selector: '.background',
+color: '#6c63fe',
+connectParticles: false,
+minDistance:100,
+maxParticles:139,
+speed:.7,
+sizeVariations:4,
+});
